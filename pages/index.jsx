6d186fcdd5fdc98cc565a25e8ae573ed90d4e55c
SandwhyDev <sandwhydev@gmail.com>
2022-07-19 17:35:23 +0700

    asdsa

--- pages/index.jsx
+++ pages/index.jsx
@@ -136,7 +136,7 @@ export default function Home() {
               title="YouTube video player"
               frameBorder={0}
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              classname="rounded-xl"
+              className="rounded-xl"
               allowFullScreen
             />
           </div>
